666f5b5022351c86fd4cd3296ed428e232fcc983
mhzed <minhongz@gmail.com>
2018-11-18 12:07:02 +0800

    remove warnings

--- src/main/java/com/mhzed/solr/join/JoinSets.java
+++ src/main/java/com/mhzed/solr/join/JoinSets.java
@@ -1,13 +1,12 @@
 package com.mhzed.solr.join;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.Set;
 
 public class JoinSets {
 
-	private List<Set<Object>> sets = new ArrayList<Set<Object>>();
-	
+	// private List<Set<Object>> sets = new ArrayList<Set<Object>>();
 	public JoinSets() {
 	}
 	
